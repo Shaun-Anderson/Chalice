b3a547bbe81e728805d4f897073a3f8628561e00
Shaun Anderson <shaun.anderson@outlook.com>
2018-05-29 00:18:29 +1000

    Updated with more UI setup (text alignment and rotations for the lower UI)

--- Overfloweth/View/CardView.swift
+++ Overfloweth/View/CardView.swift
@@ -21,22 +21,36 @@ class CardView: UIView {
     init(frame: CGRect, card: Card) {
         
         // Create UI elements
-        self.topText = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        self.topSuit = UIImageView(frame: CGRect(x: 12.5, y: 50, width: 25, height: 25))
-        self.bottomText = UILabel(frame: CGRect(x: frame.width - 100, y: frame.height - 100, width: 50, height: 50))
-        self.bottomSuit = UIImageView(frame: CGRect(x: 12.5, y: frame.height - 200, width: 25, height: 25))
+        self.topText = UILabel(frame: CGRect(x: 25, y: 25, width: 50, height: 50))
+        self.topSuit = UIImageView(frame: CGRect(x: 37.5, y: 75, width: 25, height: 25))
+        self.bottomText = UILabel(frame: CGRect(x: frame.width - 75, y: frame.height - 75, width: 50, height: 50))
+        self.bottomSuit = UIImageView(frame: CGRect(x: frame.width - 63.5, y: frame.height - 100, width: 25, height: 25))
         self.card = card
         super.init(frame: frame)
-        
+        self.backgroundColor = UIColor.white
+
         // Set UI specifcs
-        self.layer.cornerRadius = 10;
         self.topText.text = card.rank
+        self.topText.textAlignment = NSTextAlignment.center
         self.topSuit.image = UIImage(named: (card.suit?.rawValue)!)
         self.bottomText.text = card.rank
+        self.bottomText.textAlignment = NSTextAlignment.center
         self.bottomSuit.image = UIImage(named: (card.suit?.rawValue)!)
+        bottomText.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        bottomSuit.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        
+        self.addSubview(topText)
+        self.addSubview(topSuit)
+        self.addSubview(bottomText)
+        self.addSubview(bottomSuit)
+
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = 10
     }
 }
