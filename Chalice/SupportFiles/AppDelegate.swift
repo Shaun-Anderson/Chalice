79ce3edbec8e3ae5d6ddb069092e80de926d6505
Shaun Anderson <shaun.anderson@outlook.com>
2018-07-03 23:34:56 +1000

    Status bar type set globally.

--- Chalice/SupportFiles/AppDelegate.swift
+++ Chalice/SupportFiles/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.statusBarStyle = .lightContent
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = MenuViewController()
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+
         return true
     }
 
