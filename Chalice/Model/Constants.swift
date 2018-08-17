4d2e4ea0b17993e9c5ebebe7274fd43ffdf02ff2
Shaun Anderson <shaun.anderson@outlook.com>
2018-08-17 16:17:37 +1000

    Added constant file

---
+++ Chalice/Model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Chalice
+//
+//  Created by Shaun Anderson on 17/8/18.
+//  Copyright © 2018 Shaun Anderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let kingColor = UIColor(red: 255/255, green: 204/255, blue: 0/255, alpha: 1)
+
+}
